7b4f4d3c912dd7fe53ec91b698aafdefff73527d
luojeff <luojeffrey@yahoo.com>
2017-11-01 19:35:46 -0400

    add print_size method

--- makefile
+++ makefile
@@ -1,8 +1,11 @@
 app: stat.o
 	@gcc stat.o -o app
+
 main.o: main.c
 	@gcc -c stat.c
+
 run: app
 	@./app
+
 clean:
 	@rm *.o
--- stat.c
+++ stat.c
@@ -3,15 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_size(off_t size){
+  char *units[4] = {"B", "KB", "MB", "GB"};
+  int count = 0;
+
+  while(size = (size / 1000)){
+    count++;
+  }
+
+  printf("Size of file: %d%s\n", (int) size, units[count]);
+}
 
 int main(){
-  int size;
-  struct stat meta ;
   int err;
-  err = stat ("stat.c", &meta);
-  
+  struct stat meta;
+  err = stat("stat.c", &meta);  
   
   printf("Size of file: %d\n", (int) meta.st_size);
   printf("Mode of file: %o\n", (int) meta.st_mode);
   printf("Time of access: %s\n",ctime(&(meta.st_atime)) );
+
+  print_size((int) meta.st_size);
+
+
 }
